34c20b71e36d221c24189b3d4c6bcbf4e88bb371
Mark J. Blair <nf6x@nf6x.net>
2013-09-28 18:00:39 -0700

    Updated comments and version strings for distribution under GPLv3.

--- hex.h
+++ hex.h
@@ -1,9 +1,22 @@
-/* $Id: //miscsw/eprom/hex.h#1 $ */
-
 /*
  * eprom_tools: A collection of utilities for use with EPROM programmers
  *
  * Copyright (C) 1995 Mark J. Blair
+ *
+ * This file is part of eprom_tools.
+ *
+ *  eprom_tools is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  eprom_tools is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with eprom_tools.  If not, see <http://www.gnu.org/licenses/>.
  * 
  */
 
